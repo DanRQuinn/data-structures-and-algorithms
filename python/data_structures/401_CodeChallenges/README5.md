82cef597383bdcafc087855b93d03bfc80236812
Daniel Quinn <dandidit.quinn@gmail.com>
2023-06-16 16:24:25 -0700

    cc5

---
+++ python/data_structures/401_CodeChallenges/README5.md
@@ -0,0 +1,22 @@
+# Challenge 5
+
+## Linked Lists
+vd
+we traversed through linked lists using nodes and next
+
+## Contributers
+
+Dan Quinn
+Sarah Glass
+Anthony Sinitsa
+Jared Ciccerello
+BARD for writing the additional tests.
+
+## Big O
+
+Time: O(1)
+Space: O(n)
+
+## Approach & Efficiency
+
+We worked as a team going from one test to another solving each error message as it was encountered.
--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -1,15 +1,35 @@
+class Node:
+    def __init__(self, value, _next=None):
+        self.value = value
+        self.next = _next
+
 class LinkedList:
-    """
-    Put docstring here
-    """
+    def __init__(self, head=None, values=None, insert=None):
+        self.head = None
 
-    def __init__(self):
-        # initialization here
-        pass
+    def insert(self, value):
+        self.head = Node(value, self.head)
 
-    def some_method(self):
-        # method body here
-        pass
+    def __str__(self):
+        if self.head == None:
+            return "NULL"
+        else:
+            current = self.head
+            output = ""
+            while current:
+                output += f"{{ {current.value} }} -> "
+                current = current.next
+            output += "NULL"
+            return output
+
+    def includes(self, value):
+        current = self.head
+        while current:
+         if current.value == value:
+             return True
+         current = current.next
+        return False
+
 
 
 class TargetError:
